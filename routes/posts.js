1cf3de499bbf48688ceef194e921804aca4a5cd4
Ankit <ankitphondani52@gmail.com>
2019-03-25 00:16:38 +0530

    GET and ADD new post

--- routes/posts.js
+++ routes/posts.js
@@ -8,8 +8,30 @@ const Post = mongoose.model('posts');
 
 const router = express.Router();
 
-router.get('/',(req,res)=>{
+router.get('/', (req,res) => {
+    Post.find({},(err, doc) => {
+        console.log(doc);
+    });
+
     res.status(200).send();
+});
+
+router.post('/', (req, res) => {
+    
+    const post = {
+        title: req.body.title,
+        allowComments: req.body.allowComments,
+        body: req.body.body
+    }
+
+    new Post(post)
+    .save()
+    .then((doc) => {
+        console.log(doc);
+    })
+    .catch((err) => { 
+        console.log(err);
+    });
 });
 
 module.exports = router;

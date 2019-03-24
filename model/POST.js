9b5a99b0ee9d451bd33317eefdfe552c44706a43
Ankit <ankitphondani52@gmail.com>
2019-03-24 22:49:44 +0530

    Added model POST.js

---
+++ model/POST.js
@@ -0,0 +1,25 @@
+
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    },
+    allowComments : {
+        type: Boolean,
+        required: true
+    },
+    date: {
+        type: String,
+        default: Date.now
+    }
+});
+
+mongoose.model('posts', postSchema, 'posts');

752a1bf6e086cae9db371b3f38ce1f5779bed1c3
Ankit <ankitphondani52@gmail.com>
2019-03-26 21:13:38 +0530

    Improved UI and rendered posts in index page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
-
 const router = express.Router();
 
-router.get('/',(req,res) => {
-    res.render('index/index');
+router.get('/', (req, res) => {
+    res.redirect('/posts');
 });
 
 module.exports = router;
--- routes/posts.js
+++ routes/posts.js
@@ -13,6 +13,23 @@ router.get('/add', (req, res) => {
     res.render('posts/add');
 });
 
+router.get('/edit/id:', (req, res) => {
+    Post.findById(req.params.id)
+        .then((doc) => {
+            console.log(doc);
+        })
+        .catch((err) => {
+            console.log(`Error: ${err}`);
+        })
+    res.render('posts/edit');
+});
+
+
+
+/* ===========================
+========= API-ROUTES ========= 
+=========================== */
+
 // add new post
 router.post('/', (req, res) => {
     let allowComments = false;
@@ -42,9 +59,11 @@ router.post('/', (req, res) => {
 router.get('/', (req, res) => {
     Post.find({})
         .sort('-date')
-        .then((doc) => {
-            console.log(doc);
-            res.send(doc);
+        .then((posts) => {
+            // console.log(doc);
+            res.render('index/index', {
+                'posts': posts
+            });
         })
         .catch((err) => {
             res.status(404).send();

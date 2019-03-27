01238a491ce09ba2237daed9e875d9f5eec85623
Ankit <ankitphondani52@gmail.com>
2019-03-28 01:05:24 +0530

    Created index/dashboard route

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+// import Post model
+require('../model/Post');
+
+const Post = mongoose.model('posts');
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
     res.redirect('/posts');
+});
+
+router.get('/dashboard', (req, res) => {
+    Post.find({})
+        .then((posts) => {
+            // console.log(posts);
+            res.render('index/dashboard', {
+                posts
+            });
+        })
+        .catch((err) => {
+            console.log(`Error: ${err}`);
+        })
 });
 
 module.exports = router;

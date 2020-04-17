1bfe14f93cdd1c01e1a52e21467ff6ba05ee5ac6
Ankit <ankitphondani52@gmail.com>
2020-04-17 22:05:07 +0530

    Fixed issues for handlbars

--- helpers/hbs.js
+++ helpers/hbs.js
@@ -1,24 +1,24 @@
 module.exports = {
-    dateString: function (date) {
-        d = new Date(parseInt(date));
-        const newDate = d.toString().replace(/GMT.*/, '');
-        // console.log(newDate);
-        return newDate;
-    },
-    getDate: function (date) {
-        d = new Date(parseInt(date));
-        const newDate = d.toDateString();
-        // console.log(newDate);
-        return newDate;
-    },
-    select: function (selected, options) {
-        return options.fn(this).replace(new RegExp(' value=\"' + selected + '\"'), '$& selected="selected"').replace(new RegExp('>' + selected + '</option>'), ' selected="selected"$&');
-    },
-    toBase64: function (data) {
-        if (data) {
-            return data.toString('base64');
-        } else {
-            return "";
-        }
+  dateString: function (date) {
+    d = new Date(parseInt(date));
+    const newDate = d.toString().replace(/GMT.*/, '');
+    // console.log(newDate);
+    return newDate;
+  },
+  getDate: function (date) {
+    d = new Date(parseInt(date));
+    const newDate = d.toDateString();
+    // console.log(newDate);
+    return newDate;
+  },
+  select: function (selected, options) {
+    return options.fn(this).replace(new RegExp(' value=\"' + selected + '\"'), '$& selected="selected"').replace(new RegExp('>' + selected + '</option>'), ' selected="selected"$&');
+  },
+  toBase64: function (data) {
+    if (data) {
+      return data.toString('base64');
+    } else {
+      return "";
     }
+  }
 }

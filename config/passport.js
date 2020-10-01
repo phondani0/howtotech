b7381b99902049a0bfc56f8034a7d4ccd19e6ed0
SardulDhyani <sarduldhyani07@gmail.com>
2020-10-01 08:54:46 +0530

    Resolved Some issues

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const localStrategy = require('passport-local');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 // Admin model
 require('../model/Admin');
--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 // import Post model
 require('../model/Post');
